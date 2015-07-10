5d9e99d3455481a54ad6eccd03100e74ef12302e
Tobias Kräntzer <info@tobias-kraentzer.de>
2015-07-10 10:23:55 +0200

    Move the height calculation for section header and footer to the table view adapter

--- Fountain/Fountain/FTDynamicHeightTableViewAdapter.m
+++ Fountain/Fountain/FTDynamicHeightTableViewAdapter.m
@@ -12,8 +12,6 @@
 
 @interface FTDynamicHeightTableViewAdapter ()
 @property (nonatomic, readonly) NSMapTable *cellPrototypes;
-@property (nonatomic, readonly) NSMapTable *headerPrototypes;
-@property (nonatomic, readonly) NSMapTable *footerPrototypes;
 @end
 
 @implementation FTDynamicHeightTableViewAdapter
@@ -24,11 +22,7 @@
     if (self) {
         _estimatedRowHeight = UITableViewAutomaticDimension;
         _rowHeight = UITableViewAutomaticDimension;
-        _sectionHeaderHeight = UITableViewAutomaticDimension;
-        _sectionFooterHeight = UITableViewAutomaticDimension;
         _cellPrototypes = [NSMapTable weakToStrongObjectsMapTable];
-        _headerPrototypes = [NSMapTable weakToStrongObjectsMapTable];
-        _footerPrototypes = [NSMapTable weakToStrongObjectsMapTable];
     }
     return self;
 }
@@ -112,80 +106,6 @@
 
     } else {
         return tableView.rowHeight;
-    }
-}
-
-- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
-{
-    if (tableView == self.tableView) {
-
-        if (self.sectionHeaderHeight != UITableViewAutomaticDimension) {
-
-            return self.sectionHeaderHeight;
-
-        } else {
-
-            __block CGFloat height = 0;
-
-            [self headerPreperationForSection:section
-                                    withBlock:^(NSString *reuseIdentifier, FTTableViewAdapterHeaderFooterPrepareBlock prepareBlock, id item) {
-                                        if (prepareBlock) {
-
-                                            UIView *prototype = [self.headerPrototypes objectForKey:prepareBlock];
-
-                                            if (prototype == nil) {
-                                                prototype = [tableView dequeueReusableHeaderFooterViewWithIdentifier:reuseIdentifier];
-                                                [self.headerPrototypes setObject:prototype forKey:prepareBlock];
-                                            }
-
-                                            prepareBlock(prototype, item, section, self.dataSource);
-                                            CGSize targetSize = CGSizeMake(CGRectGetWidth(tableView.bounds), 0);
-                                            CGSize size = [prototype systemLayoutSizeFittingSize:targetSize];
-                                            height = size.height;
-                                        }
-                                    }];
-
-            return height;
-        }
-    } else {
-        return 0;
-    }
-}
-
-- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section
-{
-    if (tableView == self.tableView) {
-
-        if (self.sectionFooterHeight != UITableViewAutomaticDimension) {
-
-            return self.sectionFooterHeight;
-
-        } else {
-
-            __block CGFloat height = 0;
-
-            [self footerPreperationForSection:section
-                                    withBlock:^(NSString *reuseIdentifier, FTTableViewAdapterHeaderFooterPrepareBlock prepareBlock, id item) {
-                                        if (prepareBlock) {
-
-                                            UIView *prototype = [self.footerPrototypes objectForKey:prepareBlock];
-
-                                            if (prototype == nil) {
-                                                prototype = [tableView dequeueReusableHeaderFooterViewWithIdentifier:reuseIdentifier];
-                                                [self.footerPrototypes setObject:prototype forKey:prepareBlock];
-                                            }
-
-                                            prepareBlock(prototype, item, section, self.dataSource);
-                                            CGSize targetSize = CGSizeMake(CGRectGetWidth(tableView.bounds), 0);
-                                            CGSize size = [prototype systemLayoutSizeFittingSize:targetSize];
-                                            height = size.height;
-                                        }
-                                    }];
-
-            return height;
-        }
-    } else {
-        return 0;
     }
 }
 
